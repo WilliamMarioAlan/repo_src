0cf4034b85d84cb5f3364f67789fdc2b7249d118
alan <1413775276@qq.com>
2025-06-18 21:35:34 +0800

    2025.06.18

---
+++ select_chat_project/include/myfunc.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <dirent.h>
+#include <time.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <grp.h>
+
+#include <pthread.h>
+#include <sys/mman.h>
+#define ARGS_CHECK(argc,num) {if(argc!=num){perror("argc error");}}
+#define ARGC_CHECK(argc,num) {if(argc!=num){perror("argc error");}}
+#define ERROR_CHECK(ret,num,message) {if(ret==num){perror(message);return -1;}}
+
+
+#include <arpa/inet.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+#include <netdb.h>
---
+++ z_backups/nvim/lua/plugins/init.lua
@@ -0,0 +1,14 @@
+-- plugins/init.lua
+-- 插件列表定义
+
+return {
+  -- 示例插件：nvim-tree（后面我们会添加）
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   version = "*",
+  --   config = function()
+  --     require("nvim-tree").setup {}
+  --   end,
+  -- },
+}
+
